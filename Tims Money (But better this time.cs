1cb059ab7aec3e23b562fa1adbd7b040f7d681b0
NoobCaster <52872875+NoobCaster@users.noreply.github.com>
2019-09-04 21:13:33 -0400

    Add files via upload

---
+++ Tims Money (But better this time.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace myApp
+{
+    class This_Nigga_Gay // I'm black so I can say that NIGGER
+    {
+        static void Tim_is_gay_and_a_broke_boy()
+        {
+            Console.WriteLine("Ha fuck you Tim, no money for you.");
+            Console.WriteLine("and you're gay, hehe fucking fag");
+        }
+        static void Main(string[] args)
+        {
+            Tim_is_gay_and_a_broke_boy();
+        }
+    }
+}
