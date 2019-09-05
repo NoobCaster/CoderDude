a9b91752254dde6bc6cdbedcc3eb8347d9ace21d
NoobCaster <52872875+NoobCaster@users.noreply.github.com>
2019-09-04 21:17:46 -0400

    Add files via upload

---
+++ Straight up porn (but in text).cs
@@ -0,0 +1,96 @@
+using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string YourDirtyTalk;
+            string YourDirtyTalk2;
+            string WhatYouWnaSay;
+            Console.WriteLine("");
+            Console.WriteLine("Oh yes!. Fuck me more daddy");
+            Console.WriteLine("");
+            Console.WriteLine("(This is what you wanna say to her a.k.a YourDirtyTalk)");
+            Console.WriteLine("");
+
+            YourDirtyTalk = Console.ReadLine();
+            
+            Console.WriteLine("");
+            Console.WriteLine("Oh fuck yes!. Talk dirty to me *Hunyah*");
+            Console.WriteLine("");
+            
+            YourDirtyTalk2 = Console.ReadLine();
+
+            Console.WriteLine("*You take your fucking tiny dick out of her*");
+            Console.WriteLine("");
+            Console.WriteLine("Mmm lemme suck your huge dick");
+            Console.WriteLine("");
+            Console.WriteLine("Tiny*");
+            Console.WriteLine("");
+            Console.WriteLine("I'm also gonna try to count to 10 while sucking it");
+            Console.WriteLine("");
+            Console.WriteLine("I don't fucking know why but still");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+            Console.WriteLine("8");
+            Console.WriteLine("9");
+            Console.WriteLine("10");
+            Console.WriteLine("");
+            Console.WriteLine("*She stops sucking your dick*");
+            Console.WriteLine("");
+            Console.WriteLine("Want a titty fuck? :)");
+            Console.WriteLine("");
+            Console.WriteLine("You don't have a choice anyways ;)");
+            Console.WriteLine("");
+            Console.WriteLine("*She basically just gives you a titty fuck*");
+            Console.WriteLine("");
+            Console.WriteLine("*I don't have the time to write the code for the titty fuck*");
+            Console.WriteLine("");
+            Console.WriteLine("*So i'll just skip this part*");
+            Console.WriteLine("");
+            Console.WriteLine("*Anyways back to the sex*");
+            Console.WriteLine("");
+            Console.WriteLine("*You look down to realize you turned into a tentacle monster*");
+            Console.WriteLine("");
+            Console.WriteLine("Ha! *She turns into a tentacles monster* I was -");
+            Console.WriteLine("disguising myself so I could infect you with the tentacle monster virus.");
+            Console.WriteLine("");
+            Console.WriteLine("So what do you have to say?");
+            Console.WriteLine("");
+            
+            WhatYouWnaSay = Console.ReadLine();
+            
+            Console.WriteLine("");
+            Console.WriteLine("You FOOL!");
+            Console.WriteLine("");
+            Console.WriteLine("You will now die in 10 seconds *Maniacal laugh*");
+            Console.WriteLine("");
+            Console.WriteLine("Alright i'm just gonna countdown for when you die now");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+            Console.WriteLine("8");
+            Console.WriteLine("9");
+            Console.WriteLine("10");
+            Console.WriteLine("");
+            Console.WriteLine("*You fucking die*");
+            Console.WriteLine("");
+            Console.WriteLine("The End");
+            Console.WriteLine("");
+            Console.WriteLine("And yes I know that this was really really weird");
+            Console.WriteLine("");
+            Console.WriteLine("But eh");
+        }
+    }
+}
